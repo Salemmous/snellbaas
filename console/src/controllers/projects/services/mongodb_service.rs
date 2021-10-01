907bb4aeee0b95a99d8005691a2faf959bc99525
Salemmous <thomas.debluts@gmail.com>
2021-10-01 19:10:29 +0300

    Basic MongoDB CRUD operations

--- console/src/controllers/projects/services/mongodb_service.rs
+++ console/src/controllers/projects/services/mongodb_service.rs
@@ -2,7 +2,13 @@ use crate::models::project::ProjectUser;
 use crate::services::project_mongodb::ProjectMongoDBService;
 use actix_web::{http, web, HttpResponse, Responder, Scope};
 use error::SBError;
-use mongodb::{bson::Document, options::FindOptions};
+use mongodb::{
+    bson::Document,
+    options::{
+        CreateCollectionOptions, DeleteOptions, DropCollectionOptions, FindOneOptions, FindOptions,
+        InsertOneOptions, UpdateOptions,
+    },
+};
 use serde::Deserialize;
 use web::Json;
 
@@ -18,9 +24,61 @@ struct ProjectCollectionInfo {
 }
 
 #[derive(Deserialize)]
+struct ProjectDocumentInfo {
+    pub project_id: String,
+    pub collection_name: String,
+    pub document_id: String,
+}
+
+#[derive(Deserialize)]
+struct ProjectCreateCollectionQuery {
+    pub options: Option<CreateCollectionOptions>,
+}
+
+#[derive(Deserialize)]
+struct ProjectDropCollectionQuery {
+    pub options: Option<DropCollectionOptions>,
+}
+
+#[derive(Deserialize)]
 struct ProjectDocumentQuery {
     pub filter: Option<Document>,
     pub options: Option<FindOptions>,
+}
+
+#[derive(Deserialize)]
+struct ProjectCreateDocumentQuery {
+    pub document: Document,
+    pub options: Option<InsertOneOptions>,
+}
+
+#[derive(Deserialize)]
+struct ProjectGetByIdDocumentQuery {
+    pub options: Option<FindOneOptions>,
+}
+
+#[derive(Deserialize)]
+struct ProjectDeleteDocumentByIdQuery {
+    pub options: Option<DeleteOptions>,
+}
+
+#[derive(Deserialize)]
+struct ProjectDeleteDocumentQuery {
+    pub filter: Document,
+    pub options: Option<DeleteOptions>,
+}
+
+#[derive(Deserialize)]
+struct ProjectUpdateDocumentByIdQuery {
+    pub update: Document,
+    pub options: Option<UpdateOptions>,
+}
+
+#[derive(Deserialize)]
+struct ProjectUpdateDocumentQuery {
+    pub update: Document,
+    pub filter: Document,
+    pub options: Option<UpdateOptions>,
 }
 
 pub fn get_service() -> Scope {
@@ -29,8 +87,40 @@ pub fn get_service() -> Scope {
     resource
         .route("/collections", web::get().to(get_collection))
         .route(
+            "/collections/{collection_name}/create",
+            web::post().to(create_collection),
+        )
+        .route(
+            "/collections/{collection_name}/drop",
+            web::post().to(drop_collection),
+        )
+        .route(
             "/collections/{collection_name}/documents",
             web::post().to(get_documents),
+        )
+        .route(
+            "/collections/{collection_name}/documents/delete",
+            web::post().to(delete_documents),
+        )
+        .route(
+            "/collections/{collection_name}/documents/update",
+            web::post().to(update_documents),
+        )
+        .route(
+            "/collections/{collection_name}/documents/{document_id}/get",
+            web::post().to(get_document_by_id),
+        )
+        .route(
+            "/collections/{collection_name}/documents/{document_id}/delete",
+            web::post().to(delete_document_by_id),
+        )
+        .route(
+            "/collections/{collection_name}/documents/{document_id}/update",
+            web::post().to(update_document_by_id),
+        )
+        .route(
+            "/collections/{collection_name}/documents/create",
+            web::post().to(create_document),
         )
 }
 
@@ -53,17 +143,233 @@ async fn get_collection(
     }
 }
 
+async fn create_collection(
+    service: web::Data<ProjectMongoDBService>,
+    info: web::Path<ProjectCollectionInfo>,
+    options: Json<ProjectCreateCollectionQuery>,
+    _authorized_user: Option<ProjectUser>,
+) -> impl Responder {
+    let result = service
+        .create_collection(
+            &info.project_id,
+            &info.collection_name,
+            options.options.clone(),
+        )
+        .await;
+    match result {
+        Ok(_) => HttpResponse::Ok().json(true),
+        Err(SBError::ServiceError {
+            message,
+            service: _,
+        }) => HttpResponse::build(http::StatusCode::BAD_REQUEST).body(message),
+        Err(error) => {
+            println!("{}", error);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+async fn drop_collection(
+    service: web::Data<ProjectMongoDBService>,
+    info: web::Path<ProjectCollectionInfo>,
+    options: Json<ProjectDropCollectionQuery>,
+    _authorized_user: Option<ProjectUser>,
+) -> impl Responder {
+    let result = service
+        .drop_collection(
+            &info.project_id,
+            &info.collection_name,
+            options.options.clone(),
+        )
+        .await;
+    match result {
+        Ok(_) => HttpResponse::Ok().json(true),
+        Err(SBError::ServiceError {
+            message,
+            service: _,
+        }) => HttpResponse::build(http::StatusCode::BAD_REQUEST).body(message),
+        Err(error) => {
+            println!("{}", error);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
 async fn get_documents(
     service: web::Data<ProjectMongoDBService>,
     info: web::Path<ProjectCollectionInfo>,
     query: Json<ProjectDocumentQuery>,
-    authorized_user: ProjectUser,
+    _authorized_user: ProjectUser,
 ) -> impl Responder {
     let result = service
         .get_documents_from_collection(
             &info.project_id,
             &info.collection_name,
             query.filter.clone(),
+            query.options.clone(),
+        )
+        .await;
+    match result {
+        Ok(result) => HttpResponse::Ok().json(result),
+        Err(SBError::ServiceError {
+            message,
+            service: _,
+        }) => HttpResponse::build(http::StatusCode::BAD_REQUEST).body(message),
+        Err(error) => {
+            println!("{}", error);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+async fn get_document_by_id(
+    service: web::Data<ProjectMongoDBService>,
+    info: web::Path<ProjectDocumentInfo>,
+    options: Json<ProjectGetByIdDocumentQuery>,
+    _authorized_user: ProjectUser,
+) -> impl Responder {
+    let result = service
+        .get_document_by_id_from_collection(
+            &info.project_id,
+            &info.collection_name,
+            &info.document_id,
+            options.options.clone(),
+        )
+        .await;
+    match result {
+        Ok(result) => HttpResponse::Ok().json(result),
+        Err(SBError::ServiceError {
+            message,
+            service: _,
+        }) => HttpResponse::build(http::StatusCode::BAD_REQUEST).body(message),
+        Err(error) => {
+            println!("{}", error);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+async fn create_document(
+    service: web::Data<ProjectMongoDBService>,
+    info: web::Path<ProjectCollectionInfo>,
+    query: Json<ProjectCreateDocumentQuery>,
+    _authorized_user: ProjectUser,
+) -> impl Responder {
+    let result = service
+        .create_document(
+            &info.project_id,
+            &info.collection_name,
+            query.document.clone(),
+            query.options.clone(),
+        )
+        .await;
+    match result {
+        Ok(result) => HttpResponse::Ok().json(result),
+        Err(SBError::ServiceError {
+            message,
+            service: _,
+        }) => HttpResponse::build(http::StatusCode::BAD_REQUEST).body(message),
+        Err(error) => {
+            println!("{}", error);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+async fn delete_documents(
+    service: web::Data<ProjectMongoDBService>,
+    info: web::Path<ProjectCollectionInfo>,
+    query: Json<ProjectDeleteDocumentQuery>,
+    _authorized_user: ProjectUser,
+) -> impl Responder {
+    let result = service
+        .delete_documents(
+            &info.project_id,
+            &info.collection_name,
+            query.filter.clone(),
+            query.options.clone(),
+        )
+        .await;
+    match result {
+        Ok(result) => HttpResponse::Ok().json(result),
+        Err(SBError::ServiceError {
+            message,
+            service: _,
+        }) => HttpResponse::build(http::StatusCode::BAD_REQUEST).body(message),
+        Err(error) => {
+            println!("{}", error);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+async fn delete_document_by_id(
+    service: web::Data<ProjectMongoDBService>,
+    info: web::Path<ProjectDocumentInfo>,
+    query: Json<ProjectDeleteDocumentByIdQuery>,
+    _authorized_user: ProjectUser,
+) -> impl Responder {
+    let result = service
+        .delete_document(
+            &info.project_id,
+            &info.collection_name,
+            &info.document_id,
+            query.options.clone(),
+        )
+        .await;
+    match result {
+        Ok(result) => HttpResponse::Ok().json(result),
+        Err(SBError::ServiceError {
+            message,
+            service: _,
+        }) => HttpResponse::build(http::StatusCode::BAD_REQUEST).body(message),
+        Err(error) => {
+            println!("{}", error);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+async fn update_documents(
+    service: web::Data<ProjectMongoDBService>,
+    info: web::Path<ProjectCollectionInfo>,
+    query: Json<ProjectUpdateDocumentQuery>,
+    _authorized_user: ProjectUser,
+) -> impl Responder {
+    let result = service
+        .update_documents(
+            &info.project_id,
+            &info.collection_name,
+            query.filter.clone(),
+            query.update.clone(),
+            query.options.clone(),
+        )
+        .await;
+    match result {
+        Ok(result) => HttpResponse::Ok().json(result),
+        Err(SBError::ServiceError {
+            message,
+            service: _,
+        }) => HttpResponse::build(http::StatusCode::BAD_REQUEST).body(message),
+        Err(error) => {
+            println!("{}", error);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+async fn update_document_by_id(
+    service: web::Data<ProjectMongoDBService>,
+    info: web::Path<ProjectDocumentInfo>,
+    query: Json<ProjectUpdateDocumentByIdQuery>,
+    _authorized_user: ProjectUser,
+) -> impl Responder {
+    let result = service
+        .update_document(
+            &info.project_id,
+            &info.collection_name,
+            &info.document_id,
+            query.update.clone(),
             query.options.clone(),
         )
         .await;
--- console/src/services/project_mongodb.rs
+++ console/src/services/project_mongodb.rs
@@ -1,11 +1,16 @@
 use error::{SBError, SBResult};
 use futures::TryStreamExt;
+use mongodb::bson::oid::ObjectId;
 use mongodb::{
     bson::{doc, Document},
-    options::FindOptions,
-    results::CollectionSpecification,
+    options::{
+        CreateCollectionOptions, DeleteOptions, DropCollectionOptions, FindOneOptions, FindOptions,
+        InsertOneOptions, UpdateModifications, UpdateOptions,
+    },
+    results::{CollectionSpecification, DeleteResult, UpdateResult},
     Client,
 };
+use std::str::FromStr;
 
 #[derive(Clone)]
 pub struct ProjectMongoDBService {
@@ -61,5 +66,161 @@ impl ProjectMongoDBService {
                 service: String::from("mongodb"),
                 message: String::from("Failure querying documents."),
             })
+    }
+
+    pub async fn get_document_by_id_from_collection(
+        &self,
+        project_id: &str,
+        collection_name: &str,
+        document_id: &str,
+        options: Option<FindOneOptions>,
+    ) -> SBResult<Option<Document>> {
+        let oid = ObjectId::from_str(document_id).map_err(|_| SBError::InternalServiceError {
+            service: String::from("mongodb"),
+            message: String::from("Failure making oid object."),
+        })?;
+        let database = self.client.database(&format!("project-{}", project_id));
+        database
+            .collection(collection_name)
+            .find_one(doc! {"_id": oid}, options)
+            .await
+            .map_err(|_| SBError::InternalServiceError {
+                service: String::from("mongodb"),
+                message: String::from("Failure querying documents."),
+            })
+    }
+
+    pub async fn create_document(
+        &self,
+        project_id: &str,
+        collection_name: &str,
+        document: Document,
+        options: Option<InsertOneOptions>,
+    ) -> SBResult<Document> {
+        let database = self.client.database(&format!("project-{}", project_id));
+        database
+            .collection(collection_name)
+            .insert_one(document, options)
+            .await
+            .map(|r| {
+                doc! {
+                    "_id": r.inserted_id.as_object_id().unwrap().to_hex(),
+                }
+            })
+            .map_err(|_| SBError::InternalServiceError {
+                service: String::from("mongodb"),
+                message: String::from("Failure creating document."),
+            })
+    }
+
+    pub async fn create_collection(
+        &self,
+        project_id: &str,
+        collection_name: &str,
+        options: Option<CreateCollectionOptions>,
+    ) -> SBResult<()> {
+        let database = self.client.database(&format!("project-{}", project_id));
+        database
+            .create_collection(collection_name, options)
+            .await
+            .map_err(|_| SBError::InternalServiceError {
+                service: String::from("mongodb"),
+                message: String::from("Failure creating collection."),
+            })
+    }
+
+    pub async fn drop_collection(
+        &self,
+        project_id: &str,
+        collection_name: &str,
+        options: Option<DropCollectionOptions>,
+    ) -> SBResult<()> {
+        let database = self.client.database(&format!("project-{}", project_id));
+        database
+            .collection::<Document>(collection_name)
+            .drop(options)
+            .await
+            .map_err(|_| SBError::InternalServiceError {
+                service: String::from("mongodb"),
+                message: String::from("Failure creating collection."),
+            })
+    }
+
+    pub async fn delete_documents(
+        &self,
+        project_id: &str,
+        collection_name: &str,
+        filter: Document,
+        options: Option<DeleteOptions>,
+    ) -> SBResult<DeleteResult> {
+        let database = self.client.database(&format!("project-{}", project_id));
+        database
+            .collection::<Document>(collection_name)
+            .delete_many(filter, options)
+            .await
+            .map_err(|_| SBError::InternalServiceError {
+                service: String::from("mongodb"),
+                message: String::from("Failure deleting document."),
+            })
+    }
+
+    pub async fn delete_document(
+        &self,
+        project_id: &str,
+        collection_name: &str,
+        document_id: &str,
+        options: Option<DeleteOptions>,
+    ) -> SBResult<DeleteResult> {
+        let oid = ObjectId::from_str(document_id).map_err(|_| SBError::InternalServiceError {
+            service: String::from("mongodb"),
+            message: String::from("Failure making oid object."),
+        })?;
+        self.delete_documents(project_id, collection_name, doc! {"_id": oid}, options)
+            .await
+    }
+
+    pub async fn update_documents(
+        &self,
+        project_id: &str,
+        collection_name: &str,
+        filter: Document,
+        update: Document,
+        options: Option<UpdateOptions>,
+    ) -> SBResult<UpdateResult> {
+        let database = self.client.database(&format!("project-{}", project_id));
+        database
+            .collection::<Document>(collection_name)
+            .update_many(
+                filter,
+                UpdateModifications::Document(doc! {"$set": update}),
+                options,
+            )
+            .await
+            .map_err(|_| SBError::InternalServiceError {
+                service: String::from("mongodb"),
+                message: String::from("Failure updating document."),
+            })
+    }
+
+    pub async fn update_document(
+        &self,
+        project_id: &str,
+        collection_name: &str,
+        document_id: &str,
+        update: Document,
+        options: Option<UpdateOptions>,
+    ) -> SBResult<UpdateResult> {
+        let oid = ObjectId::from_str(document_id).map_err(|_| SBError::InternalServiceError {
+            service: String::from("mongodb"),
+            message: String::from("Failure making oid object."),
+        })?;
+        self.update_documents(
+            project_id,
+            collection_name,
+            doc! {"_id": oid},
+            update,
+            options,
+        )
+        .await
     }
 }
